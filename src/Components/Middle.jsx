b524e3837d2a53f1a4a57573b4d1cf1ef148c9f9
Utkarsh Kumar <Kumarutkarsh2505@gmail.com>
2022-04-29 21:26:13 +0530

    functions

--- src/Components/Middle.jsx
+++ src/Components/Middle.jsx
@@ -1,33 +1,90 @@
 import React from "react";
-import axios from "axios";
+import { v4 as uuid } from "uuid";
 import { RiDeleteBinLine } from "react-icons/ri";
 import { useState } from "react";
 import styless from "./middle.module.css";
 import styles from "./home.module.css";
 
-export const Middle = ({ isEdit }) => {
-  const [data, setData] = useState("");
+export const Middle = ({ editStatus }) => {
+  const [data, setData] = useState([{ id: 1, ruleName: "Rule1" }]);
   const [name, setName] = useState("");
   const [arr1, setArr1] = useState([1]);
 
+  const [arr2, setArr2] = useState([1]);
+
+  const handleAddNewRule = () => {
+    if (name) {
+      const payload = {
+        id: uuid(),
+        ruleName: name
+      };
+
+      setData([...data, payload]);
+      setName("");
+    }
+  };
+
+  const handleAddAnotherAction = () => {
+    if (!editStatus) {
+      if (arr2.length < 3) {
+        setArr2([...arr2, arr2.length + 1]);
+      }
+    } else {
+      alert("change to edit ");
+    }
+  };
+  const handleDelete2 = (_id) => {
+    if (!editStatus) {
+      setArr2(arr2.filter((item) => item != _id));
+    } else {
+      alert("change to edit ");
+    }
+  };
+
   const handleAddNewButton = () => {
-    setArr1([...arr1, arr1.length + 1]);
+    if (!editStatus) {
+      if (arr1.length < 8) {
+        setArr1([...arr1, arr1.length + 1]);
+      }
+    } else {
+      alert("change to edit ");
+    }
   };
 
   const handleDelete = (_id) => {
-    console.log("id", _id);
-    setArr1(arr1.filter((item) => item != _id));
+    if (!editStatus) {
+      setArr1(arr1.filter((item) => item != _id));
+    } else {
+      alert("change to edit");
+    }
   };
 
   return (
     <>
       <div className={styles.main}>
         <div className={styles.main_left}>
-          <div>
+          <div style={{ textAlign: "left", marginLeft: "1vh" }}>
             <p>Rules</p>
-            <input value={name} onChange={(e) => setName(e.target.value)} />
+
+            {editStatus ? (
+              <input
+                disabled
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            ) : (
+              <input value={name} onChange={(e) => setName(e.target.value)} />
+            )}
+
             <br />
-            <button>Add New Rule</button>
+            {data.map((item) => {
+              return (
+                <p className={styless.rule_input} key={item.id}>
+                  {item.ruleName}
+                </p>
+              );
+            })}
+            <button onClick={handleAddNewRule}>Add New Rule</button>
           </div>
         </div>
         <div className={styles.main_right}>
@@ -35,7 +92,11 @@ export const Middle = ({ isEdit }) => {
             <h4>{name}</h4>
 
             <p>Button Name</p>
-            <input type="text" />
+            {editStatus ? (
+              <input disabled type="text" />
+            ) : (
+              <input type="text" />
+            )}
 
             {arr1.map((i) => {
               return (
@@ -49,7 +110,16 @@ export const Middle = ({ isEdit }) => {
                   <select>
                     <option>Urgent</option>
                   </select>
-                  <input type="text" placeholder="Type to search  & add" />
+                  {editStatus ? (
+                    <input
+                      disabled
+                      type="text"
+                      placeholder="Type to search  & add"
+                    />
+                  ) : (
+                    <input type="text" placeholder="Type to search  & add" />
+                  )}
+
                   <RiDeleteBinLine
                     onClick={() => {
                       handleDelete(i);
@@ -68,6 +138,28 @@ export const Middle = ({ isEdit }) => {
           </div>
           <br />
           <hr />
+          <div style={{ textAlign: "left", marginLeft: "1vh" }}>
+            <p>Perform the following action:</p>
+
+            {arr2.map((i) => {
+              return (
+                <span key={i} className={styless.another_parent}>
+                  <span className={styless.another}>
+                    <p>START NEW APP</p>
+                    <p style={{ color: "blue" }}>Setup</p>
+                  </span>
+                  <RiDeleteBinLine
+                    onClick={() => {
+                      handleDelete2(i);
+                    }}
+                  />
+                </span>
+              );
+            })}
+
+            <hr />
+            <button onClick={handleAddAnotherAction}>Add another action</button>
+          </div>
         </div>
       </div>
     </>
